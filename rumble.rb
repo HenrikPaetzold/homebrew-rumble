c907807855df82dd46aca799764d16746cc5bf9c
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-07-09 16:03:06 +0200

    Update rumble.rb

--- rumble.rb
+++ rumble.rb
@@ -3,18 +3,28 @@ class Rumble < Formula
   homepage "https://rumbledb.org/"
   url "https://github.com/RumbleDB/rumble/releases/download/v2.1.0/rumbledb-2.1.0-brew.zip"
   sha256 "250b9a79e6fed34c595f75bb60d786b366e335c361169d1447538442fd32f29b"
-  license "MIT"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   depends_on "apache-spark"
 
   def install
-    lib.install Dir["*"]
-    bin.install Dir[lib/"bin/*"]
-    bin.env_script_all_files(lib/"bin", SPARK_HOME_BIN: Formula["apache-spark"].bin)
+    libexec.install "jars", "bin"
+
+    chmod 0755, libexec/"bin/rumbledb"
+
+    inreplace libexec/"bin/rumbledb", /^export JAVA_HOME=.*$/, ""
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin",
+      Language::Java.overridable_java_home_env("17").merge(SPARK_HOME_BIN: Formula["apache-spark"].bin))
   end
 
   test do
-    assert_match "2",
-      pipe_output(bin/"rumbledb -q '1+1'")
+    assert_match "2", shell_output("#{bin}/rumbledb -q '1+1'")
   end
 end
